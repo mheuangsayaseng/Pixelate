025cfa7fa150d7513eb1905db5b287237bcf588f
mheuangsayaseng <maggieheuang@gmail.com>
2023-03-06 10:33:45 -0600

    changes by maggie

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+const grid = document.getElementById("grid")
+
+function makeRow () {
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++) {
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    grid.appendChild(row)
+}
+
+// makeRow()
+// makeRow()
+
+const button = document.getElementById("add-row")
+button.addEventListener('click', makeRow)
